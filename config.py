ae20dc47935f536b3c7e8b36e2595dca54b0dcbd
nghianv5 <75850983+nghianv5@users.noreply.github.com>
2026-06-19 10:29:51 +0700

    update pass elastic

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     ELASTICSEARCH_URL = "https://zalo-room-db-f3a4ff.es.asia-southeast1.gcp.elastic.cloud:443"
-    ELASTICSEARCH_PASSWORD = "SnFhazNaNEIxY1hHWFpLdmk5STI6SXlVUDZyM1NPZEVUZHhnSEVCX0pnQQ=="
+    ELASTICSEARCH_PASSWORD = "Lytri@2505"
     ROOM_INDEX = "phong_tro_index"
     
     # Hãy đảm bảo CHỈ CÓ chuỗi ký tự Key gốc, KHÔNG CÓ dấu cách ở hai đầu
