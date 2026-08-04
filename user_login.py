ba3f54e0b8edebdd95efe5afb9d98250a4aed5be
nghianv5 <75850983+nghianv5@users.noreply.github.com>
2026-08-04 11:45:53 +0700

    Update user_login.py

--- user_login.py
+++ user_login.py
@@ -5,7 +5,7 @@ from typing import Dict, Optional
 from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+import os
 # Thêm 2 dòng này để định nghĩa templates
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
