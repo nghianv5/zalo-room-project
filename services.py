03c2b93ab0077e1abc18c3ac8ef2bd08ebbd0f6b
nghianv5 <75850983+nghianv5@users.noreply.github.com>
2026-08-12 00:54:28 +0700

    Update services.py

--- services.py
+++ services.py
@@ -81,7 +81,7 @@ try:
     qdrant_client.create_payload_index(
         collection_name=COLLECTION_NAME,
         field_name="landlord_phone",
-        field_schema=models.PayloadSchemaType.KEYWORD,
+        field_schema=qdrant_models.PayloadSchemaType.KEYWORD,
     )
 except Exception:
     pass
