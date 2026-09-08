bd5d49a4a9b0a4531803fa597780ab26297a504b
nghianv5 <75850983+nghianv5@users.noreply.github.com>
2026-09-09 03:35:54 +0700

    Update services.py

--- services.py
+++ services.py
@@ -67,7 +67,7 @@ Base = declarative_base()
 # External Clients
 qdrant_client = QdrantClient(
     url=os.environ.get("QDRANT_URL"),
-    api_key=os.environ.get("QDRANT_API_KEY")
+    api_key=os.environ.get("QDRANT_API_KEY"),
     timeout=30.0  # 👈 Thêm dòng này (đơn vị: giây)
 )
 
